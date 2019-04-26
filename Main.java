6cf6641783a7754715a2ac37bdd128b35ea20a78
ambujraj <thecoderguy@outlook.com>
2019-04-26 13:44:13 +0530

    minor

--- Main.java
+++ Main.java
@@ -11,18 +11,20 @@ class Invalid extends Exception{
 }
 
 class Account{
+public static String name="Jack";
 public static int account;
 public static String pass;
 }
 
 class Main{
 public static void main(String [] acc){
-System.out.println("\t\tWelcome to Ambuj's Banking System\t\t");
 Scanner sc = new Scanner(System.in);
 try{
 Account.account = Integer.parseInt(acc[0]);
+System.out.println("\t\tWelcome to Ambuj's Banking System - "+Account.name+"\t");
 }
 catch(Exception e){
+System.out.println("\t\tWelcome to Ambuj's Banking System\t");
 System.out.print("Account Number Invalid. Enter again:\t");
 try{
 Account.account = sc.nextInt();
@@ -32,5 +34,14 @@ System.out.println("Invalid Credentials.");
 System.exit(0);
 }
 }
+System.out.print("Enter the password:\t");
+Account.pass = sc.next();
+if(Account.pass.equals("123") && Account.account==11703203){
+    System.out.println("Success.");
+}
+else{
+    System.out.println("Invalid Credentials.");
+}
+
 }
 }
